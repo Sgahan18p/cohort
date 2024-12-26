763aca69818a056365fc66ff952599ce30cd1d99
shubhnakar gahan <sgahan18p@gmail.com>
2024-12-26 20:28:41 +0530

    updated

--- week2/nodejs/to_do.js
+++ week2/nodejs/to_do.js
@@ -38,24 +38,40 @@
   const fs = require("fs");
   const app = express();
   const port = 3000;
+  const path= require('path');
+  const cors= require('cors');
   app.use(bodyParser.json());
+  app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  }));
+  
   
   let todo = [];
   let id = 1;
-  const path = "todo.json";
+  const filepath = "todo.json";
   
   // Load todo data from file
   function loadTodo() {
-    if (fs.existsSync(path)) {
-      const data = fs.readFileSync(path);
-      todo = JSON.parse(data); 
-      id = todo.length > 0 ? Math.max(todo.map(t => t.id)) + 1 : 1;
+    if (fs.existsSync(filepath)) {
+        const data = fs.readFileSync(filepath, 'utf-8');
+        todo = JSON.parse(data);
+        if (todo.length > 0) {
+            const ids = todo.map(t => t.id).filter(id => typeof id === 'number');
+            id = ids.length > 0 ? Math.max(...ids) + 1 : 1;
+        } else {
+            id = 1;
+        }
+    } else {
+        id = 1;
     }
-  }
+}
+
+
   
   
   function saveTodos() {
-    fs.writeFileSync(path, JSON.stringify(todo, null, 2));  
+    fs.writeFileSync(filepath, JSON.stringify(todo, null, 2), "utf-8");  
   }
   
   function findIndex(arr, id) {
@@ -109,14 +125,30 @@
       res.status(404).send("ID not found");
     }
   }
-  
-  
+  function updatedTodo(req, res) {
+    const index = findIndex(todo, parseInt(req.params.id));
+    if (index !== -1) {
+      todo[index].title = req.body.title || todo[index].title;
+      todo[index].description = req.body.description || todo[index].description;
+      saveTodos();  
+      res.status(200).json(todo[index]);
+    } else {
+      res.status(404).send("ID not found");
+    }
+  }
+
   loadTodo();
+  
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'todo.html'));
+  });
   
   app.get("/todo", allTodo);
   app.get("/todo/:id", getTodoById);
   app.post("/todo", createTodo);
   app.delete("/todo/:id", deleteTodo);
+  app.put("/todo/:id", updatedTodo);
+
   
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
