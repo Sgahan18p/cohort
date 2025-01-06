14becddd88f243c3e369c73885c8296d52427f70
shubhnakar gahan <sgahan18p@gmail.com>
2025-01-06 09:55:24 +0530

    updated

--- week 3/course_app/Readme.md
+++ week 3/course_app/Readme.md
@@ -1,4 +1,4 @@
-# 📚 Course Selling Website
+<!-- # 📚 Course Selling Website
 
 ## Description
 This is a full-stack course-selling platform where:
@@ -160,4 +160,31 @@ This is a full-stack course-selling platform where:
 1. Clone the repository:
    ```bash
    git clone https://github.com/your-username/course-selling-website.git
-   cd course-selling-website
+   cd course-selling-website -->
+
+
+
+Create a course selling website
+Description
+Admins should be able to sign up
+Admins should be able to create courses
+Course has a title, description, price, and image link
+Course should be able to be published
+Admins should be able to edit courses
+Users should be able to sign up
+Users should be able to purchase courses
+Users should be able to view purchased courses
+Users should be able to view all courses
+Routes
+Admin Routes:
+POST /admin/signup Description: Creates a new admin account. Input: { username: 'admin', password: 'pass' } Output: { message: 'Admin created successfully' }
+POST /admin/login Description: Authenticates an admin. It requires the admin to send username and password in the headers. Input: Headers: { 'username': 'admin', 'password': 'pass' } Output: { message: 'Logged in successfully' }
+POST /admin/courses Description: Creates a new course. Input: Headers: { 'username': 'admin', 'password': 'pass' } Input: Body: { title: 'course title', description: 'course description', price: 100, imageLink: 'https://linktoimage.com', published: true } Output: { message: 'Course created successfully', courseId: 1 }
+PUT /admin/courses/:courseId Description: Edits an existing course. courseId in the URL path should be replaced with the ID of the course to be edited. Input: Headers: { 'username': 'admin', 'password': 'pass' } Input: Body { title: 'updated course title', description: 'updated course description', price: 100, imageLink: 'https://updatedlinktoimage.com', published: false } Output: { message: 'Course updated successfully' }
+GET /admin/courses Description: Returns all the courses. Input: Headers: { 'username': 'admin', 'password': 'pass' } Output: { courses: [ { id: 1, title: 'course title', description: 'course description', price: 100, imageLink: 'https://linktoimage.com', published: true }, ... ] } User Routes:
+User routes
+POST /users/signup Description: Creates a new user account. Input: { username: 'user', password: 'pass' } Output: { message: 'User created successfully' }
+POST /users/login Description: Authenticates a user. It requires the user to send username and password in the headers. Input: Headers: { 'username': 'user', 'password': 'pass' } Output: { message: 'Logged in successfully' }
+GET /users/courses Description: Lists all the courses. Input: Headers: { 'username': 'admin', 'password': 'pass' } Output: { courses: [ { id: 1, title: 'course title', description: 'course description', price: 100, imageLink: 'https://linktoimage.com', published: true }, ... ] }
+POST /users/courses/:courseId Description: Purchases a course. courseId in the URL path should be replaced with the ID of the course to be purchased. Input: Headers: { 'username': 'admin', 'password': 'pass' } Output: { message: 'Course purchased successfully' }
+GET /users/purchasedCourses Description: Lists all the courses purchased by the user. Input: Headers: { 'username': 'admin', 'password': 'pass' } Output: { purchasedCourses: [ { id: 1, title: 'course title', description: 'course description', price: 100, imageLink: 'https://linktoimage.com', published: true }, ... ] }
--- week 3/course_app/index.js
+++ week 3/course_app/index.js
@@ -3,13 +3,123 @@ const app = express()
 const fs=require('fs');
 const port = 3000
 
+app.use(express.json());
+//app.use(cors());
 
+var cID=1;
+let admins=[];
+let users=[];
+let courses=[];
 
+function adminAuthentication(req,res, next){
+  const username= req.headers.username;
+  const password= req.headers.password;
+  console.log("username ",username," is authenticated ");
+  const admin=admins.find(a=> a.username === username && a.password === password)
+  if(admin){
+    next()
+  }else{
+    res.status(403).json({message : "admin authentication failed !!!"});
+  }
+}
 
+function userAuthentication(req,res, next){
+  const {username,password}=req.headers;
+  console.log("username ",username," is authenticated ");
+  const user=users.find(a=> a.username === username && a.password === password)
+  if(user){
+    req.user=user;
+    next()
+  }else{
+    res.status(403).json({message : "user authentication failed !!!"});
+  }
+}
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+function adminSignup(req,res){
+  const user = req.body
+  const userExist = users.find(u => u.username === user.username)
+  if(userExist) {
+    return res.status(403).json({message: "User already exists!"})
+  }else {
+    user["purchasedCourses"] = []
+    users.push(user)
+    res.status(200).json({message: "User created successfully."})
+  }
+}
+
+function adminLogin(req,res){
+  res.json({message:"logged in successfully "})
+}
+function adminCourses(req,res){
+  const course =req.body;
+  course.id =cID++;
+  res.json({message:"course created successfully ", courseId:course.id});
+};
+
+function adminCoursesID(req, res ){
+const courseId=parseInt(req.params.courseId);
+const course=courses.find(c=> c.id === courseId)
+if(course){
+  Object.assign(course, req.body);
+  res.json({message :"course updated successfully "})
+}else{
+  res.status(404).json({message : " course not found "})
+}
+}
+
+function userSignup(req,res){
+  const user=req.body;
+  const existingUser=users.find(a => a.username === user.username);
+  if(existingUser){
+    res.status(403).json({message : "user already exists "});
+  }else{
+    admins.push(users);
+    res.json({message : 'user created successsfully '});
+  }
+}
+function userLogin(req,res){
+  res.json({message:"logged in successfully "})
+}
+function userCourses(req,res){
+  let filteredCourses = [];
+  for(let i=0; i<courses.length; i++){
+    if(courses[i].published){
+      filteredCourses.push(courses[i]);
+    }
+  } 
+  res.json({courses: filteredCourses});
+}
+function userCoursesID(req,res){
+  const courseId=Number(req.params.courseId);
+  const course=courses.find(c=> c.id === courseId && c.published);
+  if(course){
+    const user=req.user;
+    user.purchasedCourses.push(course);
+    res.json({message:"course purchased successfully "});
+  }else{
+    res.status(404).json({message:"course not found "});
+  }
+}
+function userPurchasedCourses(req,res){
+  
+}
+
+//admin
+app.post('/admin/signup',adminSignup )
+app.post('/admin/login', adminAuthentication ,adminLogin )
+app.post('/admin/courses', adminAuthentication ,adminCourses )
+app.put('/admin/courses/:courseId', adminAuthentication, adminCoursesID )
+app.get('/admin/courses',adminAuthentication,(req,res)=>{
+  res.json({course : courses});
+} )
+
+//users
+app.post('/users/signup',userSignup )
+app.post('/users/login',userAuthentication,userLogin )
+app.get('/users/courses',userAuthentication,userCourses )
+app.post('/users/courses/:courseId',userAuthentication,userCoursesID )
+app.get('/users/purchedCourses',userAuthentication,userPurchasedCourses )
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
